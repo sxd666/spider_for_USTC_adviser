1a4d6044520ce53c24756a8f3c5861d3922317cc
sxd666 <1377615418@qq.com>
2023-10-16 20:41:26 +0800

    排序类写完啦，将Infomation类中的direcions属性更名为directions属性

---
+++ model/rank.py
@@ -0,0 +1,32 @@
+import requests 
+from bs4 import BeautifulSoup
+import json
+import sqlite3
+from spider import Spider
+from spider import Infomation
+
+class Rank():
+    # ranklist为teacher的排序副本
+    def __init__(self, url, database):
+        spider = Spider(url)
+        spider.initdb(database)
+
+        # 使用元组，为了方便排序
+        self.ranklist = []
+        for info in spider.teachers:
+            self.ranklist.append((info.tid, info.college, info.major, info.name, info.page, info.visitor, info.directions))
+        
+    # info_type 为类型 0-默认 1-学院 2-专业 3-名字 5-访问量
+    def rank(self, info_type):
+        self.ranklist.sort(key=lambda x:-x[info_type])
+        return
+        
+if __name__ == "__main__":
+    content = Rank('https://dslx.ustc.edu.cn/?menu=expertlist&year=2023', '../database/teacher.db')
+    content.rank(5)
+    cs = 0
+    for info in content.ranklist:
+        cs = cs + 1
+        print(info)
+        if cs == 100:
+            break
